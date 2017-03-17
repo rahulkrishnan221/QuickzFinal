807bf909f22e6707d437cc450bb92bbc5104d2eb
rahul krishnan <rahulkrishnan221@gmail.com>
2017-03-17 17:00:03 +0530

    Multiple class created

--- app/src/main/java/com/codehex2k17/rahul/quickzfinal/login.java
+++ app/src/main/java/com/codehex2k17/rahul/quickzfinal/login.java
@@ -1,13 +1,65 @@
 package com.codehex2k17.rahul.quickzfinal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class login extends AppCompatActivity {
+    private EditText username;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+    }
+    public void checkLogin(View arg0) {
+
+
+        final String email = username.getText().toString();
+        if (!isValidEmail(email)) {
+            username.setError("Invalid Email");
+        }
+
+        final String pass = password.getText().toString();
+        if (!isValidPassword(pass)) {
+            password.setError("Too short");
+        }
+
+        if (isValidEmail(email) && isValidPassword(pass)) {
+            Button sign =(Button)findViewById(R.id.signin);
+            sign.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent(login.this,scan.class);
+                    startActivity(i);
+                }
+
+            });
+        }
+
+    }
+
+    public boolean isValidEmail(String email) {
+        String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+    public boolean isValidPassword(String pass) {
+        if (pass != null && pass.length() >= 4) {
+            return true;
+        }
+        return false;
     }
 }
