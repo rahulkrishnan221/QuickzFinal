329f0cb568348f5c55647d03d11968b45d5e0670
vidhantmatta <mattavidhant@gmail.com>
2017-03-17 13:58:59 +0530

    intro

---
+++ app/src/main/java/com/codehex2k17/rahul/quickzfinal/Intro.java
@@ -0,0 +1,97 @@
+package com.codehex2k17.rahul.quickzfinal;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+
+import com.github.paolorotolo.appintro.AppIntro;
+
+
+
+
+public class Intro extends AppIntro {
+    private SessionManagement session;
+
+
+
+
+    @Override
+    public void init(Bundle savedInstanceState) {
+        addSlide(SampleSlide.newInstance(R.layout.activity_introone));
+        addSlide(SampleSlide.newInstance(R.layout.introtwo));
+        addSlide(SampleSlide.newInstance(R.layout.introthree));
+        addSlide(SampleSlide.newInstance(R.layout.introfour));
+
+        session = new SessionManagement(getApplicationContext());
+
+        hideStatusBar();
+
+
+        setFlowAnimation();
+
+    }
+
+
+
+    private void hideStatusBar() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+
+            Window window = getWindow();
+
+            window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        }
+
+    }
+
+
+    @Override
+    public void onSkipPressed() {
+
+        session.createSplashSession();
+
+        Intent go = new Intent(Intro.this, login.class);
+
+        finish();
+
+        startActivity(go);
+
+    }
+
+    @Override
+    public void onNextPressed() {
+
+    }
+
+    @Override
+    public void onDonePressed() {
+
+        session.createSplashSession();
+
+        Intent go = new Intent(Intro.this, login.class);
+
+        finish();
+
+        startActivity(go);
+
+    }
+
+    @Override
+    public void onSlideChanged() {
+
+    }
+
+    public void getStarted(View v) {
+        Toast.makeText(getApplicationContext(), getString(R.string.app_name), Toast.LENGTH_SHORT).show();
+    }
+
+
+
+}
+
---
+++ app/src/main/java/com/codehex2k17/rahul/quickzfinal/Splash.java
@@ -0,0 +1,54 @@
+package com.codehex2k17.rahul.quickzfinal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.codehex2k17.rahul.quickzfinal.Intro;
+
+
+public class Splash extends AppCompatActivity {
+
+
+    private SessionManagement session;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        session = new SessionManagement(getApplicationContext());
+
+        redirect();
+
+    }
+
+
+
+    private void redirect() {
+
+        if (session.introDone()) {
+
+            Intent go = new Intent(this,MainActivity.class);
+
+            finish();
+
+            startActivity(go);
+
+        }
+
+        else {
+
+            Intent go = new Intent(this, Intro.class);
+
+            finish();
+
+            startActivity(go);
+
+        }
+
+    }
+
+
+}
