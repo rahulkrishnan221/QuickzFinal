ff954a176a3edd8b62a0e9767825f35621e30be6
vidhantmatta <mattavidhant@gmail.com>
2017-03-17 17:25:24 +0530

    cart backend done

---
+++ app/src/main/java/com/codehex2k17/rahul/quickzfinal/Config.java
@@ -0,0 +1,24 @@
+package com.codehex2k17.rahul.quickzfinal;
+
+import com.android.volley.toolbox.StringRequest;
+
+/**
+ * Created by vidhant on 8/3/17.
+ */
+
+public class Config {
+
+        public static final String DATA_URL = "http://www.theminimalist.esy.es/check.php?id=";
+        public static final String KEY_NAME = "Name";
+        public static final String KEY_PRICE = "Price";
+        public static final String KEY_DISCOUNT="Discount";
+        public static final String JSON_ARRAY = "result";
+        public static final String KEY_STOCK = "stock";
+        public static final String KEY_CARTURL = "http://www.theminimalist.esy.es/cart.php";
+        public static String KEY_ID = "id";
+        public static String KEY_NAME = "Name";
+        public static String KEY_PRICE = "Price";
+        public static final String JSON_ARRAYCART="result";
+        public static final String KEY_RURL= "http://www.theminimalist.esy.es/getall.php";
+
+}
--- app/src/main/java/com/codehex2k17/rahul/quickzfinal/cart.java
+++ app/src/main/java/com/codehex2k17/rahul/quickzfinal/cart.java
@@ -1,13 +1,123 @@
 package com.codehex2k17.rahul.quickzfinal;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class cart extends AppCompatActivity {
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class cart extends AppCompatActivity implements View.OnClickListener{
+    private EditText editTextId;
+    private Button buttonGet;
+    Button btnauto;
+    private TextView textViewResult;
+    public static String Name;
+    public static String Discount;
+    public static String Price;
+    public static String Finalprice;
+    public static String id;
+
+    private ProgressDialog loading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+
+
+        editTextId = (EditText) findViewById(R.id.editTextId1);
+        buttonGet = (Button) findViewById(R.id.buttonGet1);
+        textViewResult = (TextView) findViewById(R.id.textviewresult);
+
+        buttonGet.setOnClickListener(this);
+
+        Intent i = getIntent();
+        id = i.getStringExtra("TextBox");
+
+        editTextId.setText(id);
+        buttonGet.performClick();
+    }
+
+    private void getData() {
+        String id = editTextId.getText().toString().trim();
+        if (id.equals("")) {
+            Toast.makeText(this, "Please enter an id", Toast.LENGTH_LONG).show();
+            return;
+        }
+        loading = ProgressDialog.show(this, "Please wait...", "Fetching...", false, false);
+
+        String url = Config.DATA_URL + editTextId.getText().toString().trim();
+
+        StringRequest stringRequest = new StringRequest(url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                loading.dismiss();
+                showJSON(response);
+            }
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(cart.this, error.getMessage().toString(), Toast.LENGTH_LONG).show();
+                    }
+                });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+
+    }
+
+    private void showJSON(String response) {
+
+
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray result = jsonObject.getJSONArray(Config.JSON_ARRAY);
+            JSONObject Data = result.getJSONObject(0);
+            Name = Data.getString(Config.KEY_NAME);
+            Price = Data.getString(Config.KEY_PRICE);
+            Discount=Data.getString(Config.KEY_DISCOUNT);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        textViewResult.setText("Name:\t" + Name + "\nMRP:\tRs" + Price);
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+        if (v == buttonGet)
+            getData();
+        buttonclick();
+    }
+
+
+
+    private void buttonclick() {
+        btnauto = (Button) findViewById(R.id.btnauto1);
+        btnauto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in1 = new Intent(cart.this, addcart.class);
+                startActivity(in1);
+            }
+
+        });
+
     }
 }
