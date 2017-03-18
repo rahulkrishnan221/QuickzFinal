ca1bd48a08e40aea6a2d73d405650f32d45bd69d
rahul krishnan <rahulkrishnan221@gmail.com>
2017-03-18 10:32:01 +0530

    Logout using google done

--- app/src/main/java/com/codehex2k17/rahul/quickzfinal/scan.java
+++ app/src/main/java/com/codehex2k17/rahul/quickzfinal/scan.java
@@ -1,12 +1,15 @@
 package com.codehex2k17.rahul.quickzfinal;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
@@ -15,10 +18,32 @@ Button scan;
     Button cart;
     Button wallet;
     String temp;
+    private ImageButton mLogOutBtn;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard);
+        mAuth=FirebaseAuth.getInstance();
+        mAuthListener= new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                if (firebaseAuth.getCurrentUser() ==null)
+                {
+                    startActivity(new Intent(scan.this,login.class));
+                }
+
+            }
+        };
+        mLogOutBtn=(ImageButton)findViewById(R.id.logout1);
+        mLogOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+            }
+        });
         scan = (Button) findViewById(R.id.scan);
         buttonclick();
         buttonclick1();
@@ -39,6 +64,13 @@ Button scan;
 
 
     }
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
